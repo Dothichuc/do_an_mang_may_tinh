b469a14c915a20bf07ce53c4bf47a308724f7610
Dothichuc <dothichuckrp@gmail.com>
2025-11-27 01:08:28 +0700

    update code

--- src/webapp/Admin/trangadmin.php
+++ src/webapp/Admin/trangadmin.php
@@ -1,8 +1,56 @@
+<?php
+session_start(); // phải đứng đầu, trước mọi output
+
+if(!isset($_SESSION["username"])){
+    header('location:loginadmin.php');
+    exit; // luôn dùng exit sau header
+}
+
+include('connect.php');
+
+if (isset($_GET['page_layout'])){
+    switch ($_GET['page_layout']){
+        case 'quanlydanhmuc':
+            include ('danhmucsp/quanlydanhmuc.php');
+            break;
+        case 'thongtinkhachhang':
+            include ('thongtinkhachhang/thongtinkhachhang.php');
+            break;
+        case 'thongtindonhang':
+            include ('thongtindonhang/thongtindonhang.php');
+            break;
+        case 'xulyxoa':
+            include ('thongtinkhachhang/xulyxoa.php');
+            break;
+        case 'themsanpham':
+            include ('danhmucsp/themsanpham.php');
+            break;
+        case 'xoasp':
+            include ('danhmucsp/xoasp.php');
+            break;
+        case 'suasp':
+            include ('danhmucsp/suasp.php');
+            break;
+        case 'sanphamnoibat':
+            include ('sanphamnoibat/sanphamnoibat.php');
+            break;
+        case 'capnhatnoibat':
+            include ('sanphamnoibat/capnhatnoibat.php');
+            break;
+        case 'dangxuat':
+            session_destroy();
+            session_unset();
+            header('location:loginadmin.php');
+            exit;
+            break;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="admin.css" />
     <title>Admin</title>
   </head>
@@ -16,57 +64,7 @@
       </div>
       <div class="Sidebar2">
           <a href="trangadmin.php?page_layout=dangxuat">Đăng xuất</a>
-
       </div>
     </div>
-        <?php
-    session_start() ;
-    if(!isset($_SESSION["username"])){
-       header('location:loginadmin.php');
-
-    }
-     include('connect.php');
-    if (isset($_GET['page_layout'])){
-        switch ($_GET['page_layout']){
-            case 'quanlydanhmuc':
-                include ('danhmucsp/quanlydanhmuc.php');
-                break;
-            case 'thongtinkhachhang':
-                include ('thongtinkhachhang/thongtinkhachhang.php');
-                break;
-            case 'thongtindonhang':
-                include ('thongtindonhang/thongtindonhang.php');
-                break;
-            case 'xulyxoa':
-                include ('thongtinkhachhang/xulyxoa.php');
-                break;
-            case 'themsanpham':
-                include ('danhmucsp/themsanpham.php');
-                break;
-            case 'xoasp':
-                include ('danhmucsp/xoasp.php');
-                break;
-            case 'suasp':
-                include ('danhmucsp/suasp.php');
-                break;
-            case 'sanphamnoibat':
-                include ('sanphamnoibat/sanphamnoibat.php');
-                break;
-            case 'capnhatnoibat':
-                include ('sanphamnoibat/capnhatnoibat.php');
-                break;
-            case 'dangxuat':
-                session_destroy();
-                session_unset();
-                header('location:loginadmin.php');
-                break;
-        }
-    }
-    ?>
   </body>
 </html>
-
-
-
-
- 
